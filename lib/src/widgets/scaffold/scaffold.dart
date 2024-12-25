98c024238c457f37d2c9566d4405a72f4e90029f
Pavel-Notebook <lobachev-2019@mail.ru>
2024-12-25 13:36:54 +0500

    add scaffold

---
+++ lib/src/widgets/scaffold/scaffold.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class SScaffold extends StatelessWidget {
+  const SScaffold({super.key});
+  const SScaffold.scrollable({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
