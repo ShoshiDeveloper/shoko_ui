1a66170a3bae2ac29134c9482f1d257cbc6f39b7
ShoshiDeveloper-PC <lobachev-2019@mail.ru>
2025-04-03 21:51:57 +0500

    fix: STextField has new fields and fixes now

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shoko_ui/shoko_ui.dart';
 
-const whiteTheme = STheme(switchTheme: SSwitchTheme(backgroundColor: Colors.black));
-
-const darkTheme = STheme(switchTheme: SSwitchTheme(backgroundColor: Colors.red));
-
 void main() {
   runApp(const MyApp());
 }
@@ -12,74 +8,45 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: SThemeWidget(
-          shokoUITheme: STheme(
-            badgeTheme: SBadgeTheme(alertColor: Colors.red, textAlertColor: Colors.white),
-            colors: SThemeColors(
-              content: SContentColors(
-                primary: Colors.black,
-                invertPrimary: Colors.white,
-                secondary: Color.fromRGBO(155, 155, 155, 1),
-                tertiary: Color.fromRGBO(64, 64, 64, 1),
-                brand: Color.fromRGBO(126, 127, 251, 1),
-                success: Colors.green,
-                alert: Colors.red,
-              ),
-              background: SBackgroundColors(
-                  primary: Colors.white,
-                  secondary: Color.fromRGBO(250, 250, 250, 1),
-                  tertiary: Color.fromRGBO(235, 237, 240, 1),
-                  brand: Color.fromRGBO(126, 127, 251, 1),
-                  brandLight: Color.fromRGBO(229, 229, 254, 1),
-                  success: Colors.green,
-                  successLight: Color.fromRGBO(229, 255, 237, 1),
-                  alert: Colors.red,
-                  alertLight: Color.fromRGBO(255, 233, 232, 1)),
-            ),
-          ),
-          child: MyHomePage()),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool state = false;
-
-  void onChanged(bool value) {
-    setState(() {
-      state = value;
-    });
-    context.changeTheme(state ? whiteTheme : darkTheme);
-  }
-
+  // final controller = TextEditingController(text: 'djkgjkahsgkbsadbhgsbhkjgbskjhgbasdakjdasbhkjdhbashjksabhjkgs');
+  final controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: SAppBar(
-          // title: 'aboba',
-          ),
-      backgroundColor: context.theme.colors.background.secondary,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            STextField(
-              controller: TextEditingController(),
-              isError: true,
-              label: 'asdfasdf',
-            ),
-          ],
+        child: STextField(
+          label: 'label',
+          showSuffixWhenFocus: true,
+          maxLines: 1,
+          controller: controller,
+          suffix: Text('suffix'),
         ),
       ),
     );
